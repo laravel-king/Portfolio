aef99564be7ece3503dc4cbd23d615db11bf15da
laravel-king <laravelking@gmail.com>
2023-12-08 18:03:31 +0100

    Mobile navbar added successfully

--- src/components/MobileNavbar.js
+++ src/components/MobileNavbar.js
@@ -1,5 +1,26 @@
-const MobileNavbar = () => {
+import {Sheet, SheetContent, SheetTrigger} from "@/components/ui/sheet";
+import {AlignJustify} from "lucide-react";
+import AppLogo from "@/components/AppLogo";
+import Navbar from "@/components/Navbar";
+import Socials from "@/components/Socials";
 
+const MobileNavbar = () => {
+return (
+  <Sheet>
+      <SheetTrigger asChild>
+          <AlignJustify className="cursor-pointer" />
+      </SheetTrigger>
+      <SheetContent>
+          <div className="flex flex-col items-center justify-between h-full py-8">
+              <div className="flex flex-col items-center gap-y-32">
+                  <AppLogo />
+                  <Navbar containerStyles="flex flex-col items-center gap-y-6" linkStyles="text-2xl" />
+              </div>
+              <Socials containerStyles="flex gap-x-3" iconStyles="text-2xl" />
+          </div>
+      </SheetContent>
+  </Sheet>
+);
 }
 
 export default MobileNavbar;
